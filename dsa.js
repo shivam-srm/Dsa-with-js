64621a164798f1f413226b0d4ab0bb15b1377a63
Shivam-srm <shivam983840@gmail.com>
2026-01-28 22:53:37 +0530

    dsa day 1

--- dsa.js
+++ dsa.js
@@ -1,3 +1,17 @@
-let temp = 20;
-temp = 200;
-console.log(temp);
+// Swap two numbers without third variable
+let a = 10
+let b = 20 ;
+
+sum = a+b;
+console.log (a , b);
+console.log(sum);
+
+a = sum - a ;
+b = sum - 20;
+console.log (a , b);
+// 2nd method
+let c = 40
+let d = 50
+
+[ c , d ] = [  d , c ];
+console.log(c,d)

03d2be6a23989ed4640568eb1af16bddf3a9e51b
Shivam-srm <shivam983840@gmail.com>
2026-01-28 23:12:26 +0530

    even 0r odd

--- dsa.js
+++ dsa.js
@@ -1,18 +1,67 @@
-// Swap two numbers without third variable
-let a = 10
-let b = 20 ;
+// // Swap two numbers without third variable
+// let a = 10
+// let b = 20 ;
 
-sum = a+b;
-console.log (a , b);
-console.log(sum);
+// sum = a+b;
+// console.log (a , b);
+// console.log(sum);
 
-a = sum - a ;
-b = sum - 20;
-console.log (a , b);
-// 2nd method
-let c = 40;
-let d = 50;
+// a = sum - a ;
+// b = sum - 20;
+// console.log (a , b);
+// // 2nd method
+// let c = 40;
+// let d = 50;
 
-[ c , d ] = [  d , c ];
+// [ c , d ] = [  d , c ];
 
-console.log(c,d)
+// console.log(c,d)
+
+
+
+// Find largest of two numbers
+
+// let a = 20
+// let b = 10
+// if (a>b)  {
+//     console.log (`${a} is larger`)
+// }
+// else   {
+//     console.log (`${b} is larger`)
+// }
+// Find largest of three numbers
+
+let c = 10;
+let d = 20;
+let e = 30 ;
+
+if (c > d && c > e) {
+    console.log(`${c} is largest`);
+} else if (d > e) {
+    console.log(`${d} is largest`);
+} else {
+    console.log(`${e} is largest`);
+}
+
+// Check even or odd
+let  a = 10;
+let b = 3;
+if (a%2 === 0) {
+    console.log("even")
+}
+
+if (b%2===0){
+    console.log("odd")
+}
+
+// Check positive, negative, zero
+
+// Reverse a 2-digit number
+
+// Sum of digits of a number
+
+// Count digits in a number
+
+// Check number divisible by 5 and 11
+
+// Convert Celsius to Fahrenheit

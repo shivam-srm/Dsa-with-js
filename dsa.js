612cd3665c079b24dfeb88be7be79da1161968a3
Shivam-srm <shivam983840@gmail.com>
2026-02-04 23:08:16 +0530

    revert "Update dsa.js"

--- dsa.js
+++ dsa.js
@@ -161,13 +161,22 @@
 // console.log(sumeven);
 //     console.log(summodd);
 
+// let prompt =  require('prompt-sync')();
+// let n = Number(prompt("enter a number"))
+// for(let i = 1 ; i<=n ; i++){
+//     let ascii = 65 ;
+//     for(let j = 1 ; j<=i ; j++){
+//         process.stdout.write(String.fromCharCode(ascii) + " ")
+//         ascii++
+//     }
+//     console.log()
+// } 
+
 let prompt =  require('prompt-sync')();
 let n = Number(prompt("enter a number"))
-for(let i = 1 ; i<=n ; i++){
-    let ascii = 65 ;
-    for(let j = 1 ; j<=i ; j++){
-        process.stdout.write(String.fromCharCode(ascii) + " ")
-        ascii++
+for (let i = 1 ; i<= n ; i++){
+    for (let j = 5 ; j>=i  ; j--){
+         process.stdout.write( "* ")
     }
     console.log()
-} 
+}

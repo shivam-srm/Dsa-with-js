8e196e9af4521eb60e9bcd89919a2b8493fa5af5
Shivam-srm <shivam983840@gmail.com>
2026-02-04 23:28:50 +0530

    *    **   ***  **** *****

--- dsa.js
+++ dsa.js
@@ -175,8 +175,13 @@
 let prompt =  require('prompt-sync')();
 let n = Number(prompt("enter a number"))
 for (let i = 1 ; i<= n ; i++){
-    for (let j = (n+1-1) ; j>=i  ; j--){
-         process.stdout.write( "* ")
+    for (let j = (n-1) ; j>=i  ; j--){
+         process.stdout.write( " ")
     }
-    console.log()
-}
+        for (let j =  1 ; j<=i ; j++){
+            process.stdout.write("*" )
+        }
+        console.log()
+        
+    }
+    
